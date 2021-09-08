ceebdc7e22e6ccedef711b18df4173ddc8dbb14c
sudha182403 <73982774+sudha182403@users.noreply.github.com>
2021-09-08 20:02:16 +0530

    Week5 day1 assignments

---
+++ Week5.day1/BaseClassAssignment.java
@@ -0,0 +1,34 @@
+package week5.day1;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClassAssignment {
+	public ChromeDriver driver;
+
+	@BeforeMethod
+	public void Preconditions() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	
+	@AfterMethod
+	public void Postconditions() {
+		driver.close();
+	}
+}
